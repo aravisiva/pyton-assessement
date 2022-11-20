ca9d9363bd30f23f202ff9cb099924c3fca0895b
aravisiva <118651571+aravisiva@users.noreply.github.com>
2022-11-20 11:47:12 +0530

    Add files via upload

---
+++ PYTON ASSESSEMENT.py
@@ -0,0 +1,17 @@
+  #QUESTION 2
+username=input ("enter the user name")
+password= input ("enter the password")
+if (username=="aravindan" and password=="aravi"):
+  print ("Welcome Aruvi")
+else:
+  print ("wrong credentails")
+  #QUESTION 3
+number=int (input ("Values are odd or even"))
+if(number%2)==0:
+    print("It is even number")
+else:
+    print ("It is odd number")
+  #QUESTION 4
+fruits=["Mango","Orange","Watermealon"]
+for x in fruits :
+    print (x)
